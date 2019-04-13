be1b35c4eee8a0a2ae062135348a9c91b0973ab4
sultan1997 <sultan.morbiwala@gmail.com>
2019-04-13 21:25:30 +0400

    Update login.py

--- login.py
+++ login.py
@@ -4,7 +4,7 @@ import time
 driver = webdriver.Chrome()
 driver.get("https://society6.com/login?done=/")
 username = driver.find_element_by_id('email')
-username.send_keys(" ") //input your username here
+username.send_keys(" ")        #input your username here
 password = driver.find_element_by_id('password')
-password.send_keys(" ") //input your password here
+password.send_keys(" ")        #input your password here
 driver.find_element_by_name('login').click()
